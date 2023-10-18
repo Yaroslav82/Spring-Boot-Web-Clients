cd3db482edca73a3b706954c5c2818937ddf7c3d
Yaroslav82 <yargay230607@gmail.com>
2023-10-18 22:04:06 +0300

    Create Constants

---
+++ src/main/java/com/example/app/SpringBootWebClients/utils/Constants.java
@@ -0,0 +1,8 @@
+package com.example.app.SpringBootWebClients.utils;
+
+public final class Constants {
+
+    public final static String URL_IMAGES = "/images/";
+    public final static String URL_FILES = "static/files/";
+    public final static String FILE_CARS = "cars.json";
+}
