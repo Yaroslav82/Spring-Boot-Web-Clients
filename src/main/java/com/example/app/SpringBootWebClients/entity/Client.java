e9afca214d1d1e02e880131d8fc8b495508e8f1c
Yaroslav82 <yargay230607@gmail.com>
2023-10-19 10:12:41 +0300

    Add phone field

--- src/main/java/com/example/app/SpringBootWebClients/entity/Client.java
+++ src/main/java/com/example/app/SpringBootWebClients/entity/Client.java
@@ -22,6 +22,8 @@ public class Client {
     private String firstName;
     @Column(name = "last_name")
     private String lastName;
+    @Column(name = "phone")
+    private String phone;
     @Column(name = "comment")
     private String comment;
 }
