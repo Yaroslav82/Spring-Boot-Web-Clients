c35e630101f3b55329eb7be0ce14add816be8cf9
Yaroslav82 <yargay230607@gmail.com>
2023-10-19 10:12:13 +0300

    Change SQLs.sql

--- INFO/SQLs.sql
+++ INFO/SQLs.sql
@@ -5,19 +5,20 @@ CREATE TABLE IF NOT EXISTS clients
   image VARCHAR(255) NOT NULL,
   first_name VARCHAR(255) NOT NULL,
   last_name VARCHAR(255) NOT NULL,
+  phone VARCHAR(255) NOT NULL,
   comment VARCHAR(255) NOT NULL,
   PRIMARY KEY (id)
 );
 
 
 INSERT INTO
-	clients (image, first_name, last_name, comment)
+	clients (image, first_name, last_name, phone, comment)
 VALUES
-	('t1.jpg', 'Michael', 'Paul', 'Sed maximus eros quis leo congue ipsum sagittis.'),
-	('t2.jpg', 'Henry', 'Doe', 'Sed maximus eros quis leo congue ipsum sagittis.'),
-	('t3.jpg', 'Laura', 'James', 'Sed maximus eros quis leo congue ipsum sagittis.'),
-	('t4.jpg', 'Rosy', 'Crisp', 'Sed maximus eros quis leo congue ipsum sagittis.'),
-	('t5.jpg', 'Carl', 'Lii', 'Sed maximus eros quis leo congue ipsum sagittis.');
+	('t1.jpg', 'Michael', 'Paul', '555-1234567', 'Sed maximus eros quis leo congue ipsum sagittis.'),
+	('t2.jpg', 'Henry', 'Doe', '555-3451122', 'Sed maximus eros quis leo congue ipsum sagittis.'),
+	('t3.jpg', 'Laura', 'James', '555-3441135', 'Sed maximus eros quis leo congue ipsum sagittis.'),
+	('t4.jpg', 'Rosy', 'Crisp', '555-3467432', 'Sed maximus eros quis leo congue ipsum sagittis.'),
+	('t5.jpg', 'Carl', 'Lii', '555-3412454', 'Sed maximus eros quis leo congue ipsum sagittis.');
 
 
-SELECT image, first_name, last_name, comment FROM clients;
+SELECT image, first_name, last_name, phone, comment FROM clients;
