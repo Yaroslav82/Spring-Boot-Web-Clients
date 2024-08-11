13ef1670f2c888368ba3cbb8b90ee2ed668cbe46
Yaroslav82 <yargay230607@gmail.com>
2024-08-11 13:54:06 +0300

    Move SQLs.sql to flyway migrations

--- src/main/resources/db/migrations/V2__insert_clients.sql
+++ src/main/resources/db/migrations/V2__insert_clients.sql
@@ -1,16 +1,3 @@
-CREATE DATABASE demo_db;
-
-CREATE TABLE IF NOT EXISTS clients
-( id INTEGER NOT NULL AUTO_INCREMENT,
-  image VARCHAR(255) NOT NULL,
-  first_name VARCHAR(255) NOT NULL,
-  last_name VARCHAR(255) NOT NULL,
-  phone VARCHAR(255) NOT NULL,
-  comment VARCHAR(255) NOT NULL,
-  PRIMARY KEY (id)
-);
-
-
 INSERT INTO
 	clients (image, first_name, last_name, phone, comment)
 VALUES
@@ -19,6 +6,3 @@ VALUES
 	('t3.jpg', 'Laura', 'James', '555-3441135', 'Sed maximus eros quis leo congue ipsum sagittis.'),
 	('t4.jpg', 'Rosy', 'Crisp', '555-3467432', 'Sed maximus eros quis leo congue ipsum sagittis.'),
 	('t5.jpg', 'Carl', 'Lii', '555-3412454', 'Sed maximus eros quis leo congue ipsum sagittis.');
-
-
-SELECT image, first_name, last_name, phone, comment FROM clients;
